62a62cdb1e9e6f051d42eb8e83598fdde0827552
mohamed osama <mohamed.osama9812@gmail.com>
2024-01-01 10:02:45 +0200

    creating search shimmer view

---
+++ Marvel/View/ShimmerView/Search/SearchShimmerView.swift
@@ -0,0 +1,32 @@
+//
+//  SearchShimmerView.swift
+//  Marvel
+//
+//  Created by Mohamed Osama on 31/12/2023.
+//
+
+import Foundation
+import UIKit
+
+class SearchShimmerView: UIView {
+    lazy var shimmeringView: FBShimmeringView = {
+        let view = FBShimmeringView(frame: self.bounds)
+        view.isShimmering = true
+        view.shimmeringSpeed = 300
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        addSubview(shimmeringView)
+        shimmeringView.contentView = subviews.first
+
+        NSLayoutConstraint.activate([
+            shimmeringView.topAnchor.constraint(equalTo: topAnchor),
+            shimmeringView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            shimmeringView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            shimmeringView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
